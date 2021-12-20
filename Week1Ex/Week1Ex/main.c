cd4212a682612227f0c215030922aabd18063a57
Aron Wissing Kjærgaard <agk1304@gmail.com>
2021-12-20 09:58:19 +0100

    trying with hamming and ack

--- Week1Ex/Week1Ex/main.c
+++ Week1Ex/Week1Ex/main.c
@@ -1,40 +1,40 @@
- #include <stdio.h>
- #include <avr/io.h>
- #include <avr/sfr_defs.h>
- #include <stdint.h>
- // Drivers
- #include <display_7seg.h>
+#include <stdio.h>
+#include <avr/io.h>
+#include <avr/sfr_defs.h>
+#include <stdint.h>
+// Drivers
+#include <display_7seg.h>
 
- #include <ATMEGA_FreeRTOS.h>
- #include <semphr.h>
+#include <ATMEGA_FreeRTOS.h>
+#include <semphr.h>
 
- #include <FreeRTOSTraceDriver.h>
- #include <stdio_driver.h>
- #include <serial.h>
- #include <timers.h>
- #include <task.h>
+#include <FreeRTOSTraceDriver.h>
+#include <stdio_driver.h>
+#include <serial.h>
+#include <timers.h>
+#include <task.h>
 
- SemaphoreHandle_t printSemaphore;
- QueueHandle_t sendQueue;
- QueueHandle_t receiveQueue;
+SemaphoreHandle_t printSemaphore;
+QueueHandle_t sendQueue;
+QueueHandle_t receiveQueue;
 
- void receiveTask( void *pvParameters );
- void sendTask( void *pvParameters );
- void userInputTask( void *pvParameters );
- void printTask( void *pvParameters );
+void receiveTask( void *pvParameters );
+void sendTask( void *pvParameters );
+void userInputTask( void *pvParameters );
+void printTask( void *pvParameters );
 
- struct Message
- {
-	 char ucMessageID;
-	 char ucData[ 20 ];
- };
- 
+struct Message
+{
+	char ucMessageID;
+	char ucData[ 20 ];
+};
 
+typedef struct SimpleMessage SimpleMessage;
  struct SimpleMessage
- {
-	 char id;
-	 char data;
- };
+{
+	char data;
+	char parity;
+};
 
 #define QUEUE_LENGTH 10
 #define QUEUE_ITEM_SIZE sizeof(struct SimpleMessage)
@@ -58,6 +58,21 @@ char isSet(char value, int pos){
 	return value & mask;
 }
 
+// returns a new char with the least sig bit set to the value of the bit in position pos of the data parameter
+char bitAtPos(char data, int pos){
+	char mask = 0x01;
+	return (data >> (pos)) & mask;
+}
+
+// returns a char with the leas 4 bits as the hamming bits of the data
+char getHammingBits(char data){
+	// TODO: This could be prettier
+	char bit1 = bitAtPos(data, 0) ^ bitAtPos(data, 1) ^ bitAtPos(data, 3) ^ bitAtPos(data, 4) ^ bitAtPos(data, 6);
+	char bit2 = (bitAtPos(data, 0) ^ bitAtPos(data, 2) ^ bitAtPos(data, 3) ^ bitAtPos(data, 5) ^ bitAtPos(data, 6)) << 1;
+	char bit3 = (bitAtPos(data, 1) ^ bitAtPos(data, 2) ^ bitAtPos(data, 3) ^ bitAtPos(data, 7)) << 2;
+	char bit4 = (bitAtPos(data, 4) ^ bitAtPos(data, 5) ^ bitAtPos(data, 6) ^ bitAtPos(data, 7)) << 3;
+	return bit4 | bit3 | bit2 | bit1;
+}
  
 void initialiseSystem()
 {
@@ -78,9 +93,10 @@ void initialiseSystem()
 	//bit 6 and 7 also data IN
 	// DDRK = 0b10101100;
 	DDRK = 0b00110101;
+	PORTK = 0b00000000;
 
 	 
-	//PortC all data OUT and set to szero
+	//PortC all data OUT and set to zero
 	DDRC = 0b11111111;
 	PORTC = 0b00000000;
 	 
@@ -138,17 +154,16 @@ void initialiseSystem()
 }
 /*-----------------------------------------------------------*/
 
-
-
-
 int main(void)
 {
 	initialiseSystem();
 	vTaskStartScheduler();
 }
- 
- 
-char id = 0;
+
+void tryReceive(SimpleMessage message){
+
+}
+
 void receiveTask (void * pvParameters)
 {
 	//picoscope
@@ -161,15 +176,57 @@ void receiveTask (void * pvParameters)
 	
 	for(;;)
 	{
-		vTaskDelay(pdMS_TO_TICKS(100));
-		readValue = PINA;
-		if ('\0' != readValue){
-			message.data = readValue;
-			message.id = ++id;
-			xQueueSend(receiveQueue,(void*)&message, portMAX_DELAY);
+		vTaskDelay(pdMS_TO_TICKS(25));
+		
+		if ('\0' != PINA){
+			message.data = PINA;
 			vTaskDelay(pdMS_TO_TICKS(100));
+			message.parity = PINA;
+		
+			if (message.parity = getHammingBits(message.data))
+			{
+				PORTA = setBit(PORTA, 0); // ACK
+				xQueueSend(receiveQueue,(void*)&message, portMAX_DELAY);
+			}
+			else{
+				PORTA = setBit(PORTA, 2); // NACK
+			}
+		
+			vTaskDelay(pdMS_TO_TICKS(100));
+			unsetBit(PORTA, 0);
+			unsetBit(PORTA, 2);
 		}
+		
 	}
+}
+
+void trySend(SimpleMessage message){
+	PORTC = message.data;
+	vTaskDelay(pdMS_TO_TICKS(100));
+	PORTC = getHammingBits(message.data);
+	vTaskDelay(pdMS_TO_TICKS(100));
+}
+
+
+BaseType_t waitForAckNackOrTimeout(){
+	// Timeout after ~ 100ms
+	for (int i = 10; i > 0; i--)
+	{
+		if (bitAtPos(PINK, 1)) // ACK
+		{
+			return pdTRUE;
+		}
+		else if (bitAtPos(PINK, 3))
+		{
+			return pdFALSE;
+		}
+		vTaskDelay(pdMS_TO_TICKS(10));
+	}
+	return pdFALSE;
+}
+
+void clearMessage(){
+	PORTC = 0x00;
 }
 
 
@@ -180,22 +237,18 @@ void sendTask(void * pvParameters)
 	vTaskSetApplicationTaskTag(NULL, (void *) 2);
 	#endif
 	
-	char toSend = 0x41; // A
 	struct SimpleMessage message;
 	
 	for(;;)
 	{
-		//send data on pin 0 and 2
-		//printf("Sending signal \n");
-		//PORTC = 0b11111010;
 		if( xQueueReceive( sendQueue, &message, portMAX_DELAY )){
 	 
-			PORTC = message.data;
-			// Set MSG out high
-			//PORTK = setBit(PORTK, 1);
-			vTaskDelay(pdMS_TO_TICKS(200));
-			//PORTK = unsetBit(PORTK, 1);
-			PORTC = 0x00;
+			do{
+				trySend(message);
+			} while (! waitForAckNackOrTimeout());
+			
+			clearMessage();
+			vTaskDelay(pdMS_TO_TICKS(100));
 		 }
 	}
 }
@@ -222,7 +275,6 @@ void userInputTask(void * pvParameters)
 		{
 			i = getchar();
 			message.data = i;
-			message.id = ++id;
 			xQueueSend(sendQueue,(void*)&message, portMAX_DELAY);
 		}
 	}
