ae87e61265621e1e90209598d9ca046f70238a77
Aron Wissing Kjærgaard <agk1304@gmail.com>
2021-12-21 12:12:56 +0100

    cleanup and refactor

--- Week1Ex/Week1Ex/main.c
+++ Week1Ex/Week1Ex/main.c
@@ -23,32 +23,23 @@ void sendTask( void *pvParameters );
 void userInputTask( void *pvParameters );
 void printTask( void *pvParameters );
 
-struct Message
-{
-	char ucMessageID;
-	char ucData[ 20 ];
-};
-
-typedef struct SimpleMessage SimpleMessage;
- struct SimpleMessage
+typedef struct Message Message;
+ struct Message
 {
 	char data;
-	char parity;
+	char hammingCode;
 };
 
 #define QUEUE_LENGTH 10
-#define QUEUE_ITEM_SIZE sizeof(struct SimpleMessage)
+#define QUEUE_ITEM_SIZE sizeof(struct Message)
 
-TimerHandle_t timer1;
-TimerHandle_t timer2;
-TimerHandle_t timer3;
-TimerHandle_t timer4;
-
+// sets the bit in position `pos` to 1
 char setBit(char value, char pos){
 	char mask = 0x01 << pos;
 	return (value | mask);
 }
 
+// sets the bit in position `pos` to 0
 char unsetBit(char value, char pos){
 	char mask = 0x01 << pos;
 	return (value & ~mask);
@@ -59,15 +50,14 @@ char isSet(char value, int pos){
 	return value & mask;
 }
 
-// returns a new char with the least sig bit set to the value of the bit in position pos of the data parameter
+// returns a new char with the first bit set to the value of the bit in position pos of the data parameter
 char bitAtPos(char data, int pos){
 	char mask = 0x01;
 	return (data >> (pos)) & mask;
 }
 
-// returns a char with the leas 4 bits as the hamming bits of the data
+// returns a char with the least 4 bits as the hamming bits of the data
 char getHammingBits(char data){
-	// TODO: This could be prettier
 	char bit1 = bitAtPos(data, 0) ^ bitAtPos(data, 1) ^ bitAtPos(data, 3) ^ bitAtPos(data, 4) ^ bitAtPos(data, 6);
 	char bit2 = (bitAtPos(data, 0) ^ bitAtPos(data, 2) ^ bitAtPos(data, 3) ^ bitAtPos(data, 5) ^ bitAtPos(data, 6)) << 1;
 	char bit3 = (bitAtPos(data, 1) ^ bitAtPos(data, 2) ^ bitAtPos(data, 3) ^ bitAtPos(data, 7)) << 2;
@@ -78,25 +68,12 @@ char getHammingBits(char data){
 void initialiseSystem()
 {
 	// Make it possible to use stdio on COM port 0 (USB) on Arduino board - Setting 57600,8,N,1
-	 
 	stdio_initialise(ser_USART0);
 	trace_init();
 	
 	//Port initialization
 	//PortA all data IN
 	DDRA = 0b00000000;
-	 
-	//PortK
-	//bit 0 out for acknowledgment
-	//bit 1 in for acknowledgment
-	//bit 2 message out signal
-	//bit 3 message in signal
-	//bit 4 and 5 also data OUT
-	//bit 6 and 7 also data IN
-	// DDRK = 0b10101100;
-	//DDRK = 0b00110101;
-	//PORTK = 0b00000000;
-
 	 
 	//PortC all data OUT and set to zero
 	DDRC = 0b11111111;
@@ -132,7 +109,7 @@ void initialiseSystem()
 	 
 	xTaskCreate(
 	userInputTask,
-	"userInput",
+	"read",
 	configMINIMAL_STACK_SIZE,
 	NULL,
 	1,
@@ -148,10 +125,9 @@ void initialiseSystem()
 	NULL
 	);
 	
-	//Queue init
+	//Queue initialization
 	sendQueue = xQueueCreate(QUEUE_LENGTH, QUEUE_ITEM_SIZE);
 	receiveQueue = xQueueCreate(QUEUE_LENGTH, QUEUE_ITEM_SIZE);
-	
 }
 /*-----------------------------------------------------------*/
 
@@ -161,10 +137,6 @@ int main(void)
 	vTaskStartScheduler();
 }
 
-void tryReceive(SimpleMessage message){
-
-}
-
 void receiveTask (void * pvParameters)
 {
 	//picoscope
@@ -172,22 +144,19 @@ void receiveTask (void * pvParameters)
 	vTaskSetApplicationTaskTag(NULL, (void *) 2);
 	#endif
 	
-	char readValue;
-	struct SimpleMessage message;
+	struct Message message;
 	
 	for(;;)
 	{
 		vTaskDelay(pdMS_TO_TICKS(17));
 		
-		//if ('\0' != PINA && PINA > 0x0f){
-		if ('\0' != PINA){ // not hamming code
+		if ('\0' != PINA){
 			message.data = PINA;
 			vTaskDelay(pdMS_TO_TICKS(123));
-			message.parity = PINA;
+			message.hammingCode = PINA;
 		
-			if (message.parity == getHammingBits(message.data))
+			if (message.hammingCode == getHammingBits(message.data))
 			{
-				//PORTA = setBit(PORTA, 0); // ACK
 				xQueueSend(receiveQueue,(void*)&message, portMAX_DELAY);
 			}
 			else{
@@ -206,7 +175,6 @@ void receiveTask (void * pvParameters)
 		
 			vTaskDelay(pdMS_TO_TICKS(123));
 		}
-		
 	}
 }
 
@@ -218,21 +186,20 @@ void sendTask(void * pvParameters)
 	vTaskSetApplicationTaskTag(NULL, (void *) 4);
 	#endif
 	
-	struct SimpleMessage message;
+	struct Message message;
 	
 	for(;;)
 	{
 		if( xQueueReceive( sendQueue, &message, portMAX_DELAY )){
 			PORTC = message.data;
 			vTaskDelay(pdMS_TO_TICKS(100));
-			PORTC = message.parity;
+			PORTC = message.hammingCode;
 			vTaskDelay(pdMS_TO_TICKS(100));
 			PORTC = 0x00;
 			vTaskDelay(pdMS_TO_TICKS(100));
 		 }
 	}
 }
-
 
 void userInputTask(void * pvParameters)
 {
@@ -242,10 +209,10 @@ void userInputTask(void * pvParameters)
 	#endif
 	
 	TickType_t last_wake_time = xTaskGetTickCount();
-	const TickType_t xFrequency = 58 / 2; // TODO: define proper
+	const TickType_t xFrequency = 58 / 2; // supposedly around half a second
 	 
 	char i;
-	struct SimpleMessage message;
+	struct Message message;
 	 
 	for(;;)
 	{
@@ -255,7 +222,7 @@ void userInputTask(void * pvParameters)
 		{
 			i = getchar();
 			message.data = i;
-			message.parity = getHammingBits(i);
+			message.hammingCode = getHammingBits(i);
 			xQueueSend(sendQueue,(void*)&message, portMAX_DELAY);
 		}
 	}
@@ -268,15 +235,12 @@ void printTask(void * pvParameters)
 	vTaskSetApplicationTaskTag(NULL, (void *) 8);
 	#endif
 	 
-	struct SimpleMessage message;
+	struct Message message;
 	 
 	for(;;)
 	{
 		// Blocks when nothing in queue
-		// TOOD: change to receiveQueue
 		if( xQueueReceive( receiveQueue, &message, portMAX_DELAY )){
-		//if( xQueueReceive( sendQueue, &message, portMAX_DELAY )){
-	 
 			//read from the queue and print
 			if (xSemaphoreTake(printSemaphore, portMAX_DELAY))
 			{
